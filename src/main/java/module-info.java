2e25fe4693ddbf30fe573143415e9d02f53e0ce8
Luis Leal <luisrrleal@gmail.com>
2023-02-15 19:33:14 -0600

    First commit working lol

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module luisrrleal.com.registerformjava {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens luisrrleal.com.registerformjava to javafx.fxml;
+    exports luisrrleal.com.registerformjava;
+}
